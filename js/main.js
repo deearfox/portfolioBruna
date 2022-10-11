92e22ec48ed8bb9ca2adf06aadbae0bca247ca6e
Bruna Fernanda <96439119+deearfox@users.noreply.github.com>
2022-10-11 14:30:12 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+/*Email JS*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    //Check if the field has a value
+    if(contactEmail.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        // Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+    }else{
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_55mjd2c','template_rey43zh','#contact-form','cXOUenR88pyz7EIMX')
+            .then(() =>{
+                //Show message and add color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Mensagem enviada ✅'
+
+                // Remove message after five seconds
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                }, 5000 )
+            }, (error) => {
+                alert('OOPS! ALGUMA COISA FALHOU...', error)
+            })
+    }
+
+    //Show message
+    contactMessage.textContent = 'Escreva em todos os campos de input 📩'
+
+}
+contactForm.addEventListener('submit', sendEmail)
+
+/*LIGHT THEME*/
+
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'ri-sun-foggy-line'
+
+//Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.constains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'ri-sun-line' ? 'add' : 'remove'](iconTheme)
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    //Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+// Js OwlCarousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center: true, 
+        nav:true,        
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:3,
+             
+            },
+            1000:{
+                items:3,
+               
+            }
+        }
+    })
+});
